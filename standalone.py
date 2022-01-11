71f2a5119e50a3146ce71531daf401ba87b60967
Daniel Raab <daniel.raab@mailbox.org>
2022-01-11 10:15:31 +0100

    telegram bot added, sends photos after movement

--- standalone.py
+++ standalone.py
@@ -2,12 +2,19 @@
 result as files."""
 
 import plac
-# import numpy as np
 import cv2
-# import os
 from algorithm import motion_detection, get_algorithm_version
-# import file_handler
-# from evaluate_algorithm import update_frame_count
+import telegram
+
+import numpy as np
+import cv2
+
+def start_bot():
+	"""Start the bot."""
+	global bot
+	bot = telegram.Bot("2071914358:AAH_aStmNRKCehcM-GlMlU1gC5WD7sJTcRM")
+	status = bot.send_message(chat_id="-1001643202570", text="Starting doggy cam!")
+
 
 def run():
 	"""Press Ctrl-C to terminate."""
@@ -19,16 +26,19 @@ def run():
 	frame_height = int(video.get(4))
 	size = (frame_width, frame_height)
 	webcam = cv2.VideoWriter('webcam.avi', 
-	                         cv2.VideoWriter_fourcc(*'MJPG'),
-	                         10, size)
+							 cv2.VideoWriter_fourcc(*'MJPG'),
+							 10, size)
+	print('Start telegram bot...')
+	start_bot()
 
-	print('Starting doggy cam.')
+	print('Starting doggy cam...')
 	events = 0 
 	unrest = False
 	temp = None
 
 	try:
 		while True:
+			global frame
 			ret, frame = video.read()
 			if frame is None:
 				break
@@ -41,13 +51,24 @@ def run():
 			webcam.write(frame)
 
 			# detect event times and save video
+			# new unrest
 			if result and not unrest:
 				events += 1
 				temp = cv2.VideoWriter('temp_{}.avi'.format(events), 
-			                         	cv2.VideoWriter_fourcc(*'MJPG'),
-			                         	10, size)
+										cv2.VideoWriter_fourcc(*'MJPG'),
+										10, size)
+			# active unrest
 			if result:
 				temp.write(frame)
+
+			# unrest over
+			if not result and unrest:
+				cv2.imwrite('temp.png', frame)
+				global bot
+				bot.send_photo(chat_id="-1001643202570", 
+					photo=open('temp.png', 'rb'),
+					caption="doggy movement!")
+
 			unrest = result
 
 			# cv2.waitKey(1) # DEBUG
@@ -55,6 +76,7 @@ def run():
 		pass
 
 	print('Exiting doggy cam.')
+	bot.send_message(chat_id="-1001643202570", text="Stopping doggy cam!")
 	video.release()
 
 	print('{} events detected.'.format(events))
